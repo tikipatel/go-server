9193805191a4fab8d5dc7ebf97ab8c79a539378d
Pratikbhai Patel <pratikpatel@me.com>
2019-07-11 16:21:02 -0700

    got some stuff running

--- server.go
+++ server.go
@@ -1,7 +1,11 @@
 package main
 
-import "net/http"
+import (
+	"fmt"
+	"net/http"
+)
 
+// PlayerServer is a server that responds to HTTP requests
 func PlayerServer(w http.ResponseWriter, r *http.Request) {
-
+	fmt.Fprint(w, "20")
 }
--- server_test.go
+++ server_test.go
@@ -8,16 +8,17 @@ import (
 
 func TestGETPlayers(t *testing.T) {
 	t.Run("returns Pepper's score", func(t *testing.T) {
+
 		request, _ := http.NewRequest(http.MethodGet, "/players/Pepper", nil)
 		response := httptest.NewRecorder()
 
-		got := response.Body.String()
 		want := "20"
 
 		PlayerServer(response, request)
+		got := response.Body.String()
 
 		if got != want {
-			t.Errorf("got '%s', want '%s'", got, want)
+			t.Errorf("got '%q', want '%q'", got, want)
 		}
 	})
 }
