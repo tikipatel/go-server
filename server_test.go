ba36b1203a89762d706ac823c3e623c36874093f
Pratikbhai Patel <pratikpatel@me.com>
2019-07-12 14:36:48 -0700

    add new player

--- server_test.go
+++ server_test.go
@@ -273,6 +273,13 @@ func TestFileSystemStore(t *testing.T) {
 		want := 34
 		assertScoreEquals(t, got, want)
 	})
+
+	t.Run("store wins for new players", func(t *testing.T) {
+		store.RecordWin("Pepper")
+		got := store.GetPlayerScore("Pepper")
+		want := 1
+		assertScoreEquals(t, got, want)
+	})
 }
 
 func createTempFile(t *testing.T, initialData string) (io.ReadWriteSeeker, func()) {
