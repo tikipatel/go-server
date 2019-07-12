807e3eeef8829a7c3f83b37d9138b26979f24adf
Pratikbhai Patel <pratikpatel@me.com>
2019-07-11 19:58:14 -0700

    in memory

--- main.go
+++ main.go
@@ -5,8 +5,16 @@ import (
 	"net/http"
 )
 
+// InMemoryPlayerStore is a struct that will be used to hold on to player scores in memory
+type InMemoryPlayerStore struct{}
+
+// GetPlayerScore is a function that gets a player's score
+func (i *InMemoryPlayerStore) GetPlayerScore(name string) int {
+	return 123
+}
+
 func main() {
-	server := &PlayerServer{}
+	server := &PlayerServer{&InMemoryPlayerStore{}}
 	if err := http.ListenAndServe(":5000", server); err != nil {
 		log.Fatalf("could not listen on port 5000 %v", err)
 	}
--- server.go
+++ server.go
@@ -17,16 +17,7 @@ type PlayerServer struct {
 
 // ServeHTTP is a function to serve HTTP content
 func (p *PlayerServer) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Got here!! SERVEHTTP in server")
 	player := r.URL.Path[len("/players/"):]
-	fmt.Fprint(w, getPlayerScore(player))
-}
-
-func getPlayerScore(player string) (score string) {
-	if player == "Pepper" {
-		score = "20"
-	}
-	if player == "Floyd" {
-		score = "10"
-	}
-	return
+	fmt.Fprint(w, p.store.GetPlayerScore(player))
 }
